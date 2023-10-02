e31acbc9bffcf00e2fa766356b7cf4cffb94539e
Thomas Braakman 336469 <336469@student.mboutrecht.nl>
2023-10-02 14:06:20 +0200

    menu page gemaakt met login en register, route gemaakt naar menu in web.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get("/menu", function () {
+    return view("menu");
 });
 
 Route::get('/dashboard', function () {
